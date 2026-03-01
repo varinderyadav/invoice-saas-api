de4883e2158a0bb4bf6bfe931c9cb26b7cce6f1f
varinderyadav <vy849901@gmail.com>
2026-03-01 14:58:00 +0530

    creating supwruser

--- invoicesender/settings.py
+++ invoicesender/settings.py
@@ -202,3 +202,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
+if os.getenv("CREATE_SUPERUSER") == "True":
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    
+    if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
+        User.objects.create_superuser(
+            os.getenv("DJANGO_SUPERUSER_USERNAME"),
+            os.getenv("DJANGO_SUPERUSER_EMAIL"),
+            os.getenv("DJANGO_SUPERUSER_PASSWORD")
+        )
